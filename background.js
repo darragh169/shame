ea6f50dbee9904fee322291bfc23f8d63a2071c6
Darragh Bourke <darragh169@gmail.com>
2016-10-26 22:43:20 -0700

    Plays Game of Thrones .wav shame file

--- background.js
+++ background.js
@@ -23,30 +23,45 @@ chrome.tabs.onUpdated.addListener(
 	  console.log('bg.js....');
 	  console.log(arguments);
 	  if(changeInfo.url && changeInfo.url.indexOf('facebook.com') > -1){
+	  	debugger;
 	  	updateStorageObject('facebook.com');
 	  	commitToLocalStorage();
-	  	chrome.tabs.create({
-	  		index:0, 
-	  		url: chrome.extension.getURL('shame.html'), 
-	  		active: true});  	
+
+	  	if(_shameTab){
+	  		chrome.tabs.reload(_shameTab.id); 
+	  	}
+	  	else{
+	  		chrome.tabs.create({index: 0,url: chrome.extension.getURL('shame.html'),active: true}, function(tab){
+	  			_shameTab = tab;
+	  		});  
+	  	}	
 	  }
 	}
 );
 
-var _storageObj = {}
+var _storageObj = {};
+var _shameTab = null;
 
 var createJsonStorageObject = function(){
 	//_storageObj
 };
 
 var getJsonStorageObject = function(){
-
+	_storageObj = JSON.parse(localStorage.getItem('shame_shame'));
 };
 
 var updateStorageObject = function(item){
-	_storageObj[item] = 1;
+	if(_storageObj[item]){
+		var val = parseInt(_storageObj[item]);
+		_storageObj[item] = val+=1;
+	}
+	else{
+		_storageObj[item] = 1;
+	}
 };
 
 var commitToLocalStorage = function(){
 	localStorage.setItem('shame_shame', JSON.stringify(_storageObj));
 }
+
+getJsonStorageObject();
--- shame.js
+++ shame.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 
 	console.log('shame_shame');
 
+	var audio = new Audio('shame.wav');
+	audio.play();
+
 	var $shamesites = $('#shamesites');
 
 	var res = JSON.parse(localStorage.getItem('shame_shame'));
@@ -10,7 +13,5 @@ $(document).ready(function(){
   		console.log(prop + " = " + res[prop]);
   		$shamesites.append('<li>' + prop + ' ' + res[prop] + '</li>');
 	}
-
-	debugger;
 });
 
